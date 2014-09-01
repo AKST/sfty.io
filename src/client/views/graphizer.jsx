f21e5416fbe151625278734424f7bde5382a5dd9
Angus Thomsen <aksthomsen@gmail.com>
2014-09-01 21:30:36 +1000

    name space graphizer

--- src/client/views/graphizer.jsx
+++ src/client/views/graphizer.jsx
@@ -2,36 +2,42 @@
  * @jsx React.DOM
  */
 
-Sfty.View.Graphizer = React.createClass({
+Sfty.View.Graphizer = (function () {
 
-  propTypes: {
-    data: React.PropTypes.arrayOf(React.PropTypes.shape({
-      _id: React.PropTypes.number,
-      total: React.PropTypes.number
-    })).isRequired,
-    type: React.PropTypes.oneOf([
-      'bar', 'pie', 'word',
-      'bell', 'area',
-    ]).isRequired,
-  },
 
-  getDefaultProps: function(){
-    return{
-      type: null,
-      data: [],
-    };
-  },
+  
 
-  componentDidMount: function(){
-    switch (this.getDefaultProps.type) {
+  return React.createClass({
 
+    propTypes: {
+      data: React.PropTypes.arrayOf(React.PropTypes.shape({
+        _id: React.PropTypes.number,
+        total: React.PropTypes.number
+      })).isRequired,
+      type: React.PropTypes.oneOf([
+        'bar', 'pie', 'word',
+        'bell', 'area',
+      ]).isRequired,
+    },
+
+    getDefaultProps: function(){
+      return{
+        type: null,
+        data: [],
+      };
+    },
+
+    componentDidMount: function(){
+      switch (this.getDefaultProps.type) {
+
+      }
+    },
+
+    render: function () {
+      return (
+        <canvas id="aggregateChart" width="500" height="500"></canvas>
+      );
     }
-  },
 
-  render: function () {
-    return (
-      <canvas id="aggregateChart" width="500" height="500"></canvas>
-    );
-  }
-
-});
+  });
+}());
