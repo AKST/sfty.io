bdb0eca5dbf33f34719902ce78a8b27f8b458de3
Angus Thomsen <aksthomsen@gmail.com>
2014-09-02 22:39:34 +1000

    marginally clean up code

--- src/client/views/graphizer.jsx
+++ src/client/views/graphizer.jsx
@@ -4,17 +4,24 @@
 
 Sfty.View.Graphizer = (function () {
 
+  var colors = [
+    "#FF5A5E", "#FFC870", 
+    "#5AD3D1", "#CD5C5C", 
+    "#BDB76B", "#6B8E23", 
+    "#008000", "#008B8B", 
+    "#6A5ACD"
+  ];
+
 	var pieChart = function(data, ctx, category){
 		var contx = ctx.getContext("2d");
-		var colors = ["#FF5A5E", "#FFC870", "#5AD3D1", "#CD5C5C", "#BDB76B", "#6B8E23", "#008000", "#008B8B", "#6A5ACD"];
 
-		var mydata = data.map(function (x, i){
+		var mydata = data.map(function (record, index){
 			return {
-				value: x.total,
-				color: colors[i % colors.length],
+				value: record.total,
+				color: colors[index % colors.length],
 				highlight: "#DDA0DD" ,
 				label: Sfty.Config.lookupId({
-					id: x._id,
+					id: record._id,
 					category: category,
 					property: "name"
 				})
@@ -22,104 +29,62 @@ Sfty.View.Graphizer = (function () {
 		});
 
 		var options = {
-	    //Boolean - Whether we should show a stroke on each segment
 	    segmentShowStroke : true,
-
-	    //String - The colour of each segment stroke
 	    segmentStrokeColor : "white",
-
-	    //Number - The width of each segment stroke
 	    segmentStrokeWidth : 2,
-
-	    //Number - The percentage of the chart that we cut out of the middle
-	    percentageInnerCutout : 50, // This is 0 for Pie charts
-
-	    //Number - Amount of animation steps
+	    percentageInnerCutout : 50,
 	    animationSteps : 100,
-
-	    //String - Animation easing effect
 	    animationEasing : "easeOutBounce",
-
-	    //Boolean - Whether we animate the rotation of the Doughnut
 	    animateRotate : true,
-
-	    //Boolean - Whether we animate scaling the Doughnut from the centre
 	    animateScale : false,
-
-	    // Number - Tooltip label font size in pixels
 			tooltipFontSize: 11,
-
-	    //String - A legend template
-	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
 		};
 
 		// The type of char you want to draw. This case is a pie chart
 		var myPieChart = new Chart(contx).Pie(mydata, options);
 	};
 
-	var barChart = function(data, ctx){
-				var contx = document.getElementById("aggregateChart").getContext("2d");
-				var colors = ["#FF5A5E", "#FFC870", "#5AD3D1", "#CD5C5C", "#BDB76B", "#6B8E23", "#008000", "#008B8B", "#6A5ACD"];
+	var barChart = function(data, ctx, category){
+		var contx = document.getElementById("aggregateChart").getContext("2d");
 
-				var mydata = data.map(function (x, i){
-					return {
-						value: x.total,
-						label: Sfty.Config.lookupId(
-						{
-							id: x._id,
-							category: "cause",
-							property: "name"
-						})
-					};
-				});
+		var mydata = data.map(function (record){
+			return {
+				value: record.total,
+				label: Sfty.Config.lookupId({
+					id: record._id,
+					category: category,
+					property: "name"
+				})
+			};
+		});
 
-				var barlabels = _.pluck(mydata, 'label');
-				var bardatavalues = _.pluck(mydata, 'value');
+		var barlabels = _.pluck(mydata, 'label');
+		var bardatavalues = _.pluck(mydata, 'value');
 
-				var barChart = {
-				    labels: barlabels,
-				    datasets: [
-				        {
-				            label: "My First dataset",
-				            fillColor: "blue",
-				            strokeColor: "rgba(220,220,220,0.8)",
-				            highlightFill: "rgba(220,220,220,0.75)",
-				            highlightStroke: "rgba(220,220,220,1)",
-				            data: bardatavalues
-				        }
-				    ]
-				};
+		var barChart = {
+	    labels: barlabels,
+	    datasets: [{
+        label: "My First dataset",
+        fillColor: "blue",
+        strokeColor: "rgba(220,220,220,0.8)",
+        highlightFill: "rgba(220,220,220,0.75)",
+        highlightStroke: "rgba(220,220,220,1)",
+        data: bardatavalues
+		  }]
+		};
 
-				
-				var options = {
-				    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-				    scaleBeginAtZero : true,
-
-				    //Boolean - Whether grid lines are shown across the chart
-				    scaleShowGridLines : true,
-
-				    //String - Colour of the grid lines
-				    scaleGridLineColor : "rgba(0,0,0,.05)",
-
-				    //Number - Width of the grid lines
-				    scaleGridLineWidth : 1,
-
-				    //Boolean - If there is a stroke on each bar
-				    barShowStroke : true,
-
-				    //Number - Pixel width of the bar stroke
-				    barStrokeWidth : 2,
-
-				    //Number - Spacing between each of the X value sets
-				    barValueSpacing : 5,
-
-				    //Number - Spacing between data sets within X values
-				    barDatasetSpacing : 1,
-
-				    //String - A legend template
-				    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-				};
-				var myBarChart = new Chart(contx).Bar(barChart, options);
+		
+		var options = {
+	    scaleBeginAtZero : true,
+	    scaleShowGridLines : true,
+	    scaleGridLineColor : "rgba(0,0,0,.05)",
+	    scaleGridLineWidth : 1,
+	    barShowStroke : true,
+	    barStrokeWidth : 2,
+	    barValueSpacing : 5,
+	    barDatasetSpacing : 1,
+		};
+		var myBarChart = new Chart(contx).Bar(barChart, options);
 	};
 
   return React.createClass({
