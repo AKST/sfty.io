d29be3535a82ef52afd4e4c1f62f67ed68e876f9
Angus Thomsen <aksthomsen@gmail.com>
2014-10-26 20:55:04 +1000

    removes useless mixin

--- src/client/views/select.jsx
+++ src/client/views/select.jsx
@@ -2,35 +2,6 @@
  * @jsx React.DOM 
  */
 
-/**
- * Adds selectize support to select component
- */
-Sfty.View.Mixin.Selectize = {
-
-  __selectize_object: function () {
-    return $('#'+this.props.id);
-  },
-
-  /**
-   * If selectizeOptions is implemented on the
-   * object implementing the Selectize mixin,
-   * that will be called, getValue the options
-   * will just be an empty object.
-   */
-  __getOptions: function () {
-    var options = this.selectizeOptions ? 
-      this.selectizeOptions() : {};
-
-    return _.extend(options);
-  },
-
-  selectize: function () {
-    var obj = this.__selectize_object();
-    obj.selectize(this.__getOptions());
-  },
-
-};
-
 
 /**
  * Used represent a dropdown selection component
@@ -43,8 +14,6 @@ Sfty.View.Mixin.Selectize = {
  */
 Sfty.View.Select =  React.createClass({
 
-  mixins: [Sfty.View.Mixin.Selectize],
-
   getDefaultProps: function () {
     return {
       id: Sfty.Util.Rand.uid('dropdown'),
@@ -54,18 +23,14 @@ Sfty.View.Select =  React.createClass({
     };
   },
 
-  selectizeOptions: function () {
-    return {
-      onChange: this.props.onChange,
-    };
-  },
-
   componentDidMount: function () {
-    this.selectize();
+    var obj = $(this.refs.selector.getDOMNode());
+    obj.selectize({
+      onChange: this.props.onChange,
+    });
   },
 
-  render: Sfty.Util.Debug.skip("select", function () { 
-
+  render: function () { 
     return React.DOM.section({ className:"query-field" }, 
 
       <label htmlFor={this.props.id}>
@@ -81,11 +46,15 @@ Sfty.View.Select =  React.createClass({
         });
 
         elems.unshift(<option>select an option</option>);
-        elems.unshift({ id: this.props.id, defaultValue: this.props.start || undefined });
+        elems.unshift({ 
+          id: this.props.id, 
+          ref: "selector",
+          defaultValue: this.props.start || undefined 
+        });
 
         return elems;
       }).call(this))
     );
-  })
+  }
 
 });
